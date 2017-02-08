534216a6a6f62162a8b3734bec60c80921ea4325
WilliamSavage <williamsavage@hotmail.com>
2017-02-08 10:21:39 +0000

    #12 adds button to open modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -2,18 +2,14 @@
   const modal = document.querySelector("#modal");
   const modalOverlay = document.querySelector("#modal-overlay");
   const closeButton = document.querySelector("#close-button");
-  const createGoal = document.getElementById("create-goal");
-
-  createGoal.addEventListener('mouseover', () => {
-    createGoal.classList += ' show';
-  })
+  const submit = document.getElementById("submit-url");
 
   closeButton.addEventListener("click", () => {
     modal.classList.toggle("closed");
     modalOverlay.classList.toggle("closed");
   });
 
-  createGoal.addEventListener("click", () => {
+  submit.addEventListener("click", () => {
     modal.classList.toggle("closed");
     modalOverlay.classList.toggle("closed");
   });
