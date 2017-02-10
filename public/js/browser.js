50d7f9d1c70259447edfee41119b988031d9fe8c
WilliamSavage <williamsavage@hotmail.com>
2017-02-10 00:42:05 +0000

    fiddles broswer.js to take mobile to app

--- public/js/browser.js
+++ public/js/browser.js
@@ -1,3 +1,40 @@
+// Button to generate QRCode
+var qrbutton = document.querySelector('.get-qr-button');
+// QR Code container element
+var qrcode = document.querySelector('.qr-code-output');
+// Profile name element
+
+// 1. On click, call route to get QRCode svg
+var getQR = function () {
+  var o = new XMLHttpRequest();
+  o.addEventListener('load', function (e) {
+    var responseObj = JSON.parse(e.target.responseText);
+    qrbutton.style.display = 'none';
+    qrcode.innerHTML = responseObj.svg;
+    listenForToken(responseObj.proto, responseObj.url);
+  });
+  o.open('GET', '/qr');
+  o.send();
+};
+
+// 2. After SVG received, set up WebSocket with YOTI to handle response
+function listenForToken (proto, url) {
+  var host = 'wss://api.yoti.com/api/v1/connect-sessions/' + proto;
+  var socket = new WebSocket(host);
+  socket.onopen = function () {
+    socket.send(JSON.stringify({subscription: proto}));
+  };
+  // Get Token
+  socket.onmessage = function (msg) {
+    var data = JSON.parse(msg.data);
+    switch (data.status) {
+      case 'COMPLETED' : {
+        window.location = 'thankyou?token=' + data.token;
+      }
+    }
+  };
+}
+
 !function() {
     "use strict";
     var t = {
@@ -26,17 +63,33 @@
             }, r)
          },
         n = function(t) {
+            console.log('CLICKED ON MOBILE');
             /webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini|Android/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent) && i(t.currentTarget)
+        },
+
+        o = function(e, i, n) {
+          console.log('RECOGNISED MOBILE');
+           var r = t.config.qr + i,
+               o = new XMLHttpRequest;
+           o.onreadystatechange = function() {
+               if (o.readyState === XMLHttpRequest.DONE && 200 === o.status) {
+                   var t = JSON.parse(o.responseText);
+                   e.href = t.qrCodeUrl + "?callback=" + t.callbackUrl + "&id=" + t.application.id + "&mobile=" + JSON.stringify(!!n)
+               }
+           }, o.open("GET", r, !0),
+           o.setRequestHeader("X-Requested-With", "XMLHttpRequest"),
+           o.setRequestHeader("content-type", "application/json"),
+           o.send(null)
+       },
+        r = function(e, i) {
+            e.preventDefault();
+            var n = new XMLHttpRequest,
+                r = e.currentTarget,
+                o = i || t.config.qr + r.getAttribute("data-scenario-id");
+            n.onreadystatechange = function() {
+                n.readyState === XMLHttpRequest.DONE && 200 === n.status && s(n.response, r, i)
+            }, n.open("GET", o, !0), n.setRequestHeader("X-Requested-With", "XMLHttpRequest"), n.send(null)
         }
-        // r = function(e, i) {
-        //     e.preventDefault();
-        //     var n = new XMLHttpRequest,
-        //         r = e.currentTarget,
-        //         o = i || t.config.qr + r.getAttribute("data-scenario-id");
-        //     n.onreadystatechange = function() {
-        //         n.readyState === XMLHttpRequest.DONE && 200 === n.status && s(n.response, r, i)
-        //     }, n.open("GET", o, !0), n.setRequestHeader("X-Requested-With", "XMLHttpRequest"), n.send(null)
-        // },
         // o = function(e, i, n) {
         //     var r = t.config.qr + i,
         //         o = new XMLHttpRequest;
@@ -95,50 +148,33 @@
             u = l.getAttribute("data-yoti-application-id"),
             p = l.getAttribute("data-yoti-scenario-id"),
             b = l.getAttribute("data-yoti-type"),
-            g = document.createElement("a"),
+            // g gets the href set in o function - this will fire off need to add event listener
+
+            g = qrbutton,
             m = document.createElement("img"),
-            A = a(l.dataset.size),
+              // A = a(l.dataset.size),
             v = t.config.service + u;
-            /webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini|Android/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent) ? (b = "_self", o(g, p, !0)) : (v = "javascript:void(0)", n = r);
-        }
-    }, window._ybg = t;
-  }();
-    // Button to generate QRCode
-    var qrbutton = document.querySelector('.get-qr-button');
-    // QR Code container element
-    var qrcode = document.querySelector('.qr-code-output');
-    // Profile name element
+            //HERE WE CHECK IF WE ARE ON A MOBILE OR NOT!
+            /webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini|Android/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent) ? (b = "_self", o(g, p, !0)) : (v = "javascript:void(0)", n = getQR);
 
-    // 1. On click, call route to get QRCode svg
-    qrbutton.addEventListener('click', function () {
-      var o = new XMLHttpRequest();
-      o.addEventListener('load', function (e) {
-        var responseObj = JSON.parse(e.target.responseText);
-        qrbutton.style.display = 'none';
-        qrcode.innerHTML = responseObj.svg;
-        listenForToken(responseObj.proto, responseObj.url);
-      });
-      o.open('GET', '/qr');
-      o.send();
-    });
+          g.setAttribute("href", t.config.service + u),
+          g.setAttribute("data-application-id", u),
+          g.setAttribute("data-scenario-id", p),
+          console.log(g, 'THE BUTTON');
+          g.addEventListener("click", n);
 
-    // 2. After SVG received, set up WebSocket with YOTI to handle response
-    function listenForToken (proto, url) {
-      var host = 'wss://api.yoti.com/api/v1/connect-sessions/' + proto;
-      var socket = new WebSocket(host);
-      socket.onopen = function () {
-        socket.send(JSON.stringify({subscription: proto}));
-      };
-      // Get Token
-      socket.onmessage = function (msg) {
-        var data = JSON.parse(msg.data);
-        switch (data.status) {
-          case 'COMPLETED' : {
-            window.location = 'thankyou?token=' + data.token;
-          }
         }
-      };
+
     }
+    t.config.service = 'https://code.yoti.com/app/'
+    t.init()
+    // window._ybg = t;
+
+
+
+  }();
+
+
 
   //   function getProfile (token) {
   //   var o = new XMLHttpRequest();
